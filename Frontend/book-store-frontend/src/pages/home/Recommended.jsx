ea39130d187a0c82e87025681cd5eb2bee164a0b
addygit123 <minecraftaddy123@gmail.com>
2025-03-08 12:51:33 +0530

    Fix case sensitivity issue for bookcard

--- Frontend/book-store-frontend/src/pages/home/Recommended.jsx
+++ Frontend/book-store-frontend/src/pages/home/Recommended.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Bookcard from "../books/Bookcard";
+import Bookcard from "../books/BookCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Pagination } from "swiper/modules";
